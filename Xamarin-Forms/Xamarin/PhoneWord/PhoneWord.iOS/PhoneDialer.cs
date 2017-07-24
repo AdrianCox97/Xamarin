0fe3b03d99685dd463dfcaef18037478b21ce971
AdrianCox97 <street_cox1997@hotmail.com>
2017-07-24 14:11:07 -0500

    Actualización

---
+++ Xamarin-Forms/Xamarin/PhoneWord/PhoneWord.iOS/PhoneDialer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using PhoneWord.iOS;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+namespace PhoneWord.iOS
+{
+    class PhoneDialer : IDialer
+    {
+
+    }
+}
--- Xamarin-Forms/Xamarin/PhoneWord/PhoneWord/MainPage.xaml.cs
+++ Xamarin-Forms/Xamarin/PhoneWord/PhoneWord/MainPage.xaml.cs
@@ -9,9 +9,42 @@ namespace PhoneWord
 {
     public partial class MainPage : ContentPage
     {
+        String numeroTraducido;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        void btnTraducir_Traducir(object sender, EventArgs e)
+        {
+            numeroTraducido = Core.Traductor.ToNumber(txtNumeroTelefono.Text);
+            if (!String.IsNullOrWhiteSpace(numeroTraducido))
+            {
+                btnLlamar.IsEnabled = true;
+                btnLlamar.Text = $"Llamar a { numeroTraducido }";
+            }
+            else
+            {
+                btnLlamar.IsEnabled = false;
+                btnLlamar.Text = "Llamar";
+            }
+        }
+
+        async void btnLlamar_Llamar(object sender, EventArgs e)
+        {
+            if (await this.DisplayAlert (
+                "Llamar a Número",
+                $"¿Te gustaría llamar al número { numeroTraducido }?",
+                "Yes",
+                "No"))
+            {
+                var marcador = DependencyService.Get<IDialer>();
+                if (marcador != null)
+                {
+                    marcador.Dial(numeroTraducido);
+                }
+            }
         }
     }
 }
