f424d7978dcd738c150a42170f3f29d9f41ff5e5
AdrianCox97 <street_cox1997@hotmail.com>
2017-07-25 11:35:55 -0500

    Actualización

---
+++ Xamarin-Forms/XamarinForms-App/Phoneword/Phoneword.iOS/PhoneDialer.cs
@@ -0,0 +1,24 @@
+﻿using Foundation;
+using Phoneword.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Phoneword;
+using System;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+namespace Phoneword.iOS
+{
+    public class PhoneDialer : IDialer
+    {
+        public bool Dial(string number)
+        {
+            return UIApplication.SharedApplication.OpenUrl(
+                new NSUrl($"tel:{ number }"));
+        }
+    }
+
+    public interface IDialer
+    {
+        bool Dial(string number);
+    }
+}
